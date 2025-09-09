6891743f919ff9058da17b4ec958655760f2d20f
AramBP <aramvanderpols05@gmail.com>
2025-09-09 22:10:30 +0200

    implemented policies module

--- src/markov_processes/Policies.jl
+++ src/markov_processes/Policies.jl
@@ -1,6 +1,8 @@
 module Policies
 
-export Policy, DeterministicPolicy, act
+using RL.DistributionsExt: Constant, Choose
+
+export Policy, DeterministicPolicy, UniformPolicy, act
 
 abstract type Policy{S, A} end
 
@@ -8,9 +10,15 @@ act(policy::Policy) = error("The function act is not defined for $(typeof(policy
 
 
 struct DeterministicPolicy{S, A} <: Policy{S, A}
-    action_for::Function
+    action_for::Function # f: S -> A
 end
 
-#act(policy::DeterministicPolicy, state::NonTerminal) = Constant(policy.action_for(state.state))
+act(policy::DeterministicPolicy, state::NonTerminal) = Constant(policy.action_for(state.state))
+
+struct UniformPolicy{S, A} <: Policy{S, A}
+    valid_actions::Function # f: S -> Array{A}
+end
+
+act(policy::UniformPolicy, state::NonTerminal) = Choose(policy.valid_actions(state.state))
 
 end
