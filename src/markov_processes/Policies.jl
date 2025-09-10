b09088057c2e7946e1c6d52464cd1b54db32bec3
AramBP <aramvanderpols05@gmail.com>
2025-09-10 12:34:18 +0200

    fixed incorrect type of parameter

--- src/markov_processes/Policies.jl
+++ src/markov_processes/Policies.jl
@@ -9,7 +9,7 @@ abstract type Policy{S, A} end
 abstract type DeterministicPolicy{S, A} <: Policy{S, A} end
 abstract type UniformPolicy{S, A} <: Policy{S, A} end
 
-act(policy::Policy) = error("The function act is not defined for $(typeof(policy))")
+act(policy::Policy, s::NonTerminal) = error("The function act is not defined for $(typeof(policy))")
 action_for(policy::Policy, s::NonTerminal) = error("The function action_for is not defined for $(typeof(policy))")
 
 act(policy::DeterministicPolicy, state::NonTerminal) = Constant(policy.action_for(state.state))
