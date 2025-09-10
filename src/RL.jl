77e02eaa912d3c706055108f50eb0ab24e10289a
AramBP <aramvanderpols05@gmail.com>
2025-09-10 12:34:36 +0200

    added the MDP module to the RL module

--- src/RL.jl
+++ src/RL.jl
@@ -1,10 +1,11 @@
 module RL
 
-export DistributionsExt, MarkovProcesses, MarkovRewardProcesses, Policies
+export DistributionsExt, MarkovProcesses, MarkovRewardProcesses, Policies, MarkovDecisionProcesses
 
 include("DistributionsExt.jl")
 include("markov_processes/MarkovProcesses.jl")
 include("markov_processes/MarkovRewardProcesses.jl")
 include("markov_processes/Policies.jl")
+include("markov_processes/MarkovDecisionProcesses.jl")
 
 end
