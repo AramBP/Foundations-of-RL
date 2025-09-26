8899fb59fc0addde152325d994c5fe1c622e580a
AramBP <aramvanderpols05@gmail.com>
2025-09-26 15:25:39 +0200

    implemented finite MDP

--- src/markov_processes/MarkovDecisionProcesses.jl
+++ src/markov_processes/MarkovDecisionProcesses.jl
@@ -2,12 +2,12 @@ module MarkovDecisionProcesses
 
 using Distributions
 
-using RL.MarkovProcesses: NonTerminal, State
+using RL.MarkovProcesses: NonTerminal, State, Terminal
 using RL.Policies: Policy, act
-using RL.DistributionsExt: apply
+using RL.DistributionsExt: apply, FiniteDistribution, SampledDistribution, LabeledCategorical
 using RL.MarkovRewardProcesses: MarkovRewardProcess
 
-export MarkovDecisionProcess, actions, step, transition_reward, ImpliedMRP, simulate_actions
+export MarkovDecisionProcess, actions, step, transition_reward, ImpliedMRP, simulate_actions, FiniteMarkovDecisionProcess
 
 struct TransitionStep{S, A}
     state::NonTerminal{S}
@@ -53,6 +53,35 @@ function simulate_actions(mdp::MarkovDecisionProcess, start_states::Distribution
         state = next_state
     end
     return sample_trace
+end
+
+struct FiniteMarkovDecisionProcess{S, A} <: MarkovDecisionProcess{S, A}
+    mapping::Dict{NonTerminal{S}, Dict{A, FiniteDistribution}}
+    non_terminals::Vector{S}
+end
+
+function FiniteMarkovDecisionProcess(transition_mapping::Dict{S, Dict{A, T}}) where {S, A, T <: FiniteDistribution{Tuple{S, Float64}}}
+    non_terminals_transition_mapping = collect(keys(transition_mapping))
+    mapping = Dict(NonTerminal(s) => {a => LabeledCategorical(
+        Dict(((s1 in non_terminals_transition_mapping) ? NonTerminal(s1) : Terminal(s1), r) => p) for ((s1, r), p) in v
+    ) for (a, v) in d} for (s, d) in transition_mapping)
+    non_terminals_mapping = collect(keys(mapping))
+    return FiniteMarkovDecisionProcess(mapping, non_terminals_mapping)
+end
+
+function Base.show(io::IO, fmdp::FiniteMarkovDecisionProcess)
+    display = ""
+    for (s, d) in fmdp.mapping
+        display *= "From State $(s.state): \n"
+        for (a, d1) in d
+            display *= "  With Action $(a): \n"
+            for ((s1, r), p) in d1 
+                opt = isa(s1, Terminal) ? "Terminal " : ""
+                display *= "  To [$(opt) State $(s1.state) and Reward $(round(r; digits = 3))] with Probability $(round(p; digits = 3))" 
+            end
+        end
+    end
+    print(io, display)
 end
 
 end
