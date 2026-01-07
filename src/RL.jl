cf44e12016153369979bc0a8f169165621a262bc
Aram Brouwer <aramvanderpols05@gmail.com>
2026-01-07 14:48:33 +0100

    preemptive commit before refactoring

--- src/RL.jl
+++ src/RL.jl
@@ -1,11 +1,13 @@
 module RL
 
-export DistributionsExt, MarkovProcesses, MarkovRewardProcesses, Policies, MarkovDecisionProcesses
+export DistributionsExt, MarkovProcesses, MarkovRewardProcesses, Policies, MarkovDecisionProcesses,
+    Iterate
 
 include("DistributionsExt.jl")
 include("markov_processes/MarkovProcesses.jl")
 include("markov_processes/MarkovRewardProcesses.jl")
 include("markov_processes/Policies.jl")
 include("markov_processes/MarkovDecisionProcesses.jl")
+include("dynamic_programming/Iterate.jl")
 
 end
